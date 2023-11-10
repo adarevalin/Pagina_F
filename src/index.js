ea0127b77eb3cad46f34f181f9c8cd6644b5e0d1
Andres David <kyfunky1386@gmail.com>
2023-11-09 19:04:43 -0500

    v1

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {createRoot} from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.css'
+
+
+const root = createRoot(document.getElementById('root'))
+root.render (
+    <>
+    <h1>Hola Mundishoooo xd</h1>
+    </>
+)
